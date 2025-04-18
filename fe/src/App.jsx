c8749ee763790dc1cc6c0f896930b8cb0c03b50a
Arjun <arjun.ambavane@gmail.com>
2025-04-18 15:40:02 +0530

    Init frontend

---
+++ fe/src/App.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
+import { Toaster } from "./components/ui/sonner";
+import { LandingPage } from "./pages/LandingPage";
+import DashboardPage from "./pages/DashboardPage";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY =
+  "pk_test_YWxsb3dlZC1oYXdrLTM0LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+function App() {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <>
+                  <SignedIn>
+                    <DashboardPage />
+                  </SignedIn>
+                  <SignedOut>
+                    <RedirectToSignIn />
+                  </SignedOut>
+                </>
+              }
+            />
+          </Routes>
+          <Toaster />
+        </Router>
+      </ClerkProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
